b0bb564b09f6257f48d09076214506fc938c59ab
ILYAS1288 <elibubalteen@gmail.com>
2024-08-10 01:07:05 +0500

    react changes

--- p1/src/App.jsx
+++ p1/src/App.jsx
@@ -29,18 +29,18 @@ function App() {
     <div style={appStyle}>  
  
   <Header/> 
-<h1>Hello in react first class today  </h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, quasi.</p>
+<h1   style={{ textAlign: 'center' }} > My Gun pages view </h1>
+<p style={{ fontSize: '16px', color: 'lightblue' , textAlign: 'center' }}> Proper maintenance is crucial for safe and reliable gun operation.</p>
 
-<span>08/06/2024 </span>
-<br /> <br />
+<span style={{ display: 'block', textAlign: 'center' }} >08/06/2024 </span>
+<br /> 
 
- <br/>
- <p>Maintenance and Repair: Proper maintenance is crucial for safe and reliable gun operation. <br /> This includes regular cleaning to remove residue and ensure that all moving parts are functioning smoothly. <br /> Common tools used for gun maintenance include cleaning rods, brushes, and lubricants. If you're looking to repair a gun, <br /> it’s important to have a good understanding of its mechanics and to follow safety procedures.</p>
+ 
+ <p  style={{ fontSize: '19px', color: 'white' , textAlign: 'center'}} >Maintenance and Repair: Proper maintenance is crucial for safe and reliable gun operation. <br /> This includes regular cleaning to remove residue and ensure that all moving parts are functioning smoothly. <br /> Common tools used for gun maintenance include cleaning rods, brushes, and lubricants. If you're looking to repair a gun, <br /> it’s important to have a good understanding of its mechanics and to follow safety procedures.</p>
   <img className="img" src="https://images.unsplash.com/photo-1482649671545-bc53dcf1ad7c?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt=""   />
-    <h5>Gun </h5>
-    <p >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" >WWW.//bfhvbhfvbf</a>
+    <h5 style={{ fontSize: '16px', color: 'blue' , textAlign: 'center'}} >Gun </h5>
+    <p style={{ fontSize: '16px', color: 'blue', textAlign: 'center' }} > Common tools used for gun maintenance include cleaning rods, brushes, and lubricants. If you're looking to repair a gun.</p>
+    <a href="#"  style={{ display: 'block', textAlign: 'center', color: 'blue' }}  >WWW.//bfhvbhfvbf</a>
   <br /><br />
 
  <Button/>
