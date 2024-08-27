40ae02c1ffcb9c0e84d8a8a591f7f9a5f0365fa5
ILYAS1288 <elibubalteen@gmail.com>
2024-08-28 00:22:03 +0500

    react@#2

--- p1/src/App.jsx
+++ p1/src/App.jsx
@@ -8,8 +8,6 @@ import './styles/Img.css'
 import './styles/Header.css'
 import './styles/Footer.css'
 function App() {
-
-  // background image
   const appStyle = {
     backgroundImage: 'url("https://images.unsplash.com/photo-1482649671545-bc53dcf1ad7c?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
     backgroundSize: 'cover',
